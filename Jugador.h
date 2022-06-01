152f5a5f387ca33baba6d378d7e0a687eb21c79a
joacomelli <106452464+joacomelli@users.noreply.github.com>
2022-06-01 20:34:32 -0300

    Update Jugador.h

--- Jugador.h
+++ Jugador.h
@@ -2,11 +2,14 @@
 #define JUGADOR_H_
 
 #include <string>
+#include "Ficha.h"
+#include "Lista.h"
 
 class Jugador {
 private:
 	unsigned int numero;
 	std::string nombre;
+	Lista<Ficha*> * fichas; 
 
 public:
 	//PRE: El numero debe ser mayor o igual a 1, y el nombre no puede estar vacio.
@@ -24,6 +27,19 @@ public:
 	//PRE: El jugador debe existir.
 	//POST: Muestra el numero del jugador.
 	int getNumero() const;
+	
+	//PRE: El jugador debe existir.
+	//POST: Devuelve un puntero a la lista de fichas del jugador.
+	Lista<Ficha*> * getFichas();
+	
+	//PRE: La ficha recibida no debe ser NULL.
+	//POST: Si la lista de fichas esta vacia la crea y agrega la ficha. Si la lista ya esta creada agrega la ficha al principio.
+	void agregarFicha(Ficha* ficha);
+	
+	//PRE: La ficha recibida no debe ser NULL y debe pertenecer a la lista de fichas.
+	//POST: Elimina el nodo de la lista correspondiente a esa ficha.
+	void eliminarFicha(Ficha* ficha);
+	
 };
 
 #endif
