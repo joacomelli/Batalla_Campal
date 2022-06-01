f99bcd3c111f558fa025e0573b40ce622eaf1b93
joacomelli <106452464+joacomelli@users.noreply.github.com>
2022-06-01 20:34:44 -0300

    Update Jugador.cpp

--- Jugador.cpp
+++ Jugador.cpp
@@ -3,9 +3,11 @@
 Jugador::Jugador(unsigned int numero, std::string nombre) {
 	this->numero = numero;
 	this->nombre = nombre;
+	this->fichas  = NULL;
 }
 
 Jugador::~Jugador() {
+	//falta lista.h tiene que destruir la lista de fichas
 
 }
 
@@ -16,3 +18,28 @@ const std::string& Jugador::getNombre() const {
 int Jugador::getNumero() const {
 	return numero;
 }
+
+Lista<Ficha*> * Jugador::getFichas(){
+	return this->fichas;
+}
+
+void Jugador::agregarFicha(Ficha* ficha){
+	//falta lista.h
+	if(ficha == NULL){
+		throw "La ficha debe existir";
+	}
+	//agregar al principio
+}	
+
+void Jugador::eliminarFicha(Ficha* ficha){
+	if(ficha == NULL){
+		throw "La ficha debe existir";
+	}
+	//eliminar dato  de lista.h
+}
+	
+}
+
+
+
+Lista
