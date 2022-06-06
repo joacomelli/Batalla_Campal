f0f41072b4cc2374e84cd970024b5a8ae0992221
RichmondAlan <106933252+RichmondAlan@users.noreply.github.com>
2022-06-06 15:46:49 -0300

    Update Tablero.cpp

--- Tablero.cpp
+++ Tablero.cpp
@@ -1,6 +1,9 @@
 #include "Tablero.h"
 #include "Casilla.h"
 
+
+// TABLERO CON VECTOR ------------- BORRAR -----------------------
+/*
 Tablero::Tablero(unsigned int xMaximo, unsigned int yMaximo, unsigned int zMaximo){
 	if(xMaximo < 1){
 		throw " X no puede ser menor a 1";
@@ -25,6 +28,63 @@ Tablero::Tablero(unsigned int xMaximo, unsigned int yMaximo, unsigned int zMaxim
 		}
 	}
 }
+*/
+
+//TABLERO CON LISTA --------------- CASILLA RECIBE (X,Y, Z, GENERAL, --> Y UN PUNTERO A FICHA )  ---  UTILIZAR ESTE -----------
+// cambiar lo que recibe casilla a lo ultimo.
+Tablero::Tablero(unsigned int xMaximo, unsigned int yMaximo, unsigned int zMaximo){
+	if(xMaximo < 1){
+		throw " X no puede ser menor a 1";
+	}
+	if(yMaximo < 1){
+		throw " Y no puede ser menor a 1";
+	}
+	if(zMaximo < 1){
+		throw " Z no puede ser menor a 1";
+	}
+	this->xMaximo = xMaximo;
+	this->yMaximo = yMaximo;
+	this->zMaximo = zMaximo;
+	this->casilleros = new Lista<Lista<Lista<Casilla *> *> *> ();
+	for(int x = 1; x <= this->xMaximo; x++){
+		this->casilleros->agregar(new Lista<Lista<Casilla *> *>());
+		for(int y = 1; y <= this->yMaximo; y++){
+			this->casilleros->obtener(x)->agregar(new Lista<Casilla *> ());
+			for(int z = 1; z <= this->zMaximo; z++){
+				this->casilleros->obtener(x)->obtener(y)->agregar(new Casilla(x,y,z,General/*,Soldado*/));
+			}
+		}
+	}
+}
+
+//
+// NO TIENE QUE ESTAR ACA...       MUESTRA LA PANTALLA, ------ REVISAR -----
+// REVISAR SI ES "GET ESTADO" O ES OTRO GET EL QUE HAY QUE MOSTRAR
+void Tablero::imprimirTablero(){
+	Casilla * casillero;
+	this->casilleros->iniciarCursor();
+	for(int x = 1; x <= this->xMaximo; x++){
+		this->casilleros->avanzarCursor();
+		Lista<Lista<Casilla *>*>* columna = this->casilleros->obtenerCursor();
+		columna->iniciarCursor();
+		for(int y = 1; y <= this->yMaximo; y++){
+			columna->avanzarCursor();
+			Lista<Casilla *>* fila = columna->obtenerCursor();
+			fila->iniciarCursor();
+			for(int z = 1; z <= this->zMaximo; z++){
+				fila->avanzarCursor();
+				if(fila->obtenerCursor()->getFicha() == NULL){
+					std::cout <<' ';
+				}else{
+					std::cout << fila->obtenerCursor()->getFicha()->getEstado();
+				}
+				std::cout <<"|";
+			}
+			std::cout << std::endl;
+	    }
+		std::cout << std::endl << std::endl;
+	}
+}
 
 Tablero::~Tablero() {
 	for(int x = 1; x <= this->xMaximo; x++){
@@ -39,12 +99,29 @@ Tablero::~Tablero() {
 	delete this->casilleros;
 }
 
+int Tablero::getxMaximo(){
+	return this->xMaximo;
+}
+int Tablero::getyMaximo(){
+	return this->yMaximo;
+}
+
+int Tablero::getzMaximo(){
+	return this->zMaximo;
+}
+
+bool Tablero::existeElCasillero(unsigned int x, unsigned int y, unsigned int z){
+	try{
+		validarRango(x,y,z);
+		return true;
+	}catch(...){}
+		return false;
+}
 
 Casilla * Tablero::getCasillero(unsigned int x, unsigned int y, unsigned int z){
 	validarRango(x,y,z);
 	return this->casilleros->obtener(x)->obtener(y)->obtener(z);
 }
-
 
 void Tablero::validarRango(unsigned int x, unsigned int y, unsigned int z){
 	if(x < 1 && x > this->xMaximo){
