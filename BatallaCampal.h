632b0741b8bfd2a4eb3fce64f77cc43b07a5ebeb
RichmondAlan <106933252+RichmondAlan@users.noreply.github.com>
2022-06-08 16:59:04 -0300

    Update BatallaCampal.h

--- BatallaCampal.h
+++ BatallaCampal.h
@@ -39,7 +39,7 @@ private:
 public:
 
 	//PRE:-
-	//POST:
+	//POST: Inicializa el tablero en NULL y los jugadores en NULL
 	BatallaCampal();
 
 	//PRE:
@@ -47,54 +47,55 @@ public:
 	virtual ~BatallaCampal();
 
 	//PRE:-
-	//POST: Inicia el tablero en el escenario 1 pre configurado.
+	//POST: Inicia el tablero en el escenario 1 pre configurado con sus maximos.
 	void iniciarEscenarioUno(unsigned int xMaximo, unsigned int yMaximo, unsigned int zMaximo);
 
 	//PRE:-
-	//POST: Inicia el tablero en el escenario 2 pre configurado.
+	//POST: Inicia el tablero en el escenario 2 pre configurado con sus maximos.
 	void iniciarEscenarioDos(unsigned int xMaximo, unsigned int yMaximo, unsigned int zMaximo);
 
 	//PRE:-
 	//POST: Pide por consola una coordenada (x,y,z) y la retorna como vector dinamico.
 	Vector<unsigned int> * pedirCoordenadas();
 	 
-	//
-	//
+	// PRE: Debe existir una ficha.
+	// POS: Devuelve la ficha que se ubica en la posicion que se pasa por parametro.
 	Ficha * obtenerFicha( unsigned int x, unsigned int y, unsigned int z);
 	
-	//
-	//
+	//PRE: Debe existir la casilla 
+	//POST: Devuelve el estado en el que se encuentra la casilla que se pasa por parametro.
 	EstadoDeCasilla obtenerEstadoDeCasilla(unsigned int x, unsigned int y, unsigned int z);
 	
-	//
-	//
+	//PRE: Debe existir la casilla 
+	//POST: Devuelve el tipo de casilla que se pasa por parametro.
 	TipoDeCasilla obtenerTipoDeCasilla(unsigned int x, unsigned int y, unsigned int z);
 	
-	//
-	//
+	//PRE: Debe existir un jugador y las coordenadas validas. 
+	//POST: Devolvera true si hay un soldado en las posiciones pasadas por parametro, false si no hay un soldado. 
 	bool tieneUnSoldado(Jugador * jugador, unsigned int x, unsigned int y, unsigned int z);
 	
 	//
 	//
 	Vector<unsigned int> * pedirOrigenDelMovimiento(Jugador *jugador);
 
-	//
-	//
+	//PRE: Debe existir el jugador.
+	//POST: Se le pedira al jugador que ingrese coordenadas donde desee mover su jugador, comparara la posicion si hay otro soldado,
+	//      si la casilla no es tierra y si la casilla no esta inactiva, retorta el destino del soldado.
 	Vector<unsigned int> * pedirDestinoDelMovimiento(Jugador *jugador);
 
-	//
-	//
+	//PRE: Debe existir el jugador
+	//POST: Pedira al jugador coordenadas para mover sus fichas. 
 	Vector<Vector<unsigned int> *> * pedirMovimiento(Jugador *jugador);
 
-	//
-	//
+	//PRE: -
+	//POST: Destruira las coordenadas que se les pase por consola el jugador.
 	void destruirCoordenadasDelMovimiento(Vector<Vector<unsigned int> *> * coordenadas);
 	
 	//
 	//
 	void mover(Vector<Vector<unsigned int> *> * coordenadasOrigenYDestino);
 
-	//
+	//PRE: Recibe coordendas en
 	//
 	bool movimientoCercano(Vector<unsigned int> * origen, Vector<unsigned int> * destino);
 	
