36cebda262583669e9fa637afd85c407e8e16b04
agusfffff <106780131+agusfffff@users.noreply.github.com>
2022-06-09 19:53:14 -0300

    Update imagen.h

--- imagen.h
+++ imagen.h
@@ -1,1 +1,24 @@
+#ifndef IMAGEN_H_
+#define IMAGEN_H_
 
+#include "EasyBMP.h"
+
+class Imagen {
+  private:
+	  BMP background; 
+    BMP inactiva;
+    BMP avion; 
+    BMP barco;
+    BMP soldado;
+    BMP estado;
+    void medidaDeEstado();
+    void agregarFondoDeEstado(); 
+
+public:
+  Imagen();
+  void definirMapa(std::string background); 
+  void obtenerEstado();
+  
+};
+
+#endif
