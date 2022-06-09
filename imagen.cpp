e5f579cef60e2ba1bf590e01491e92102a7779a1
agusfffff <106780131+agusfffff@users.noreply.github.com>
2022-06-09 19:53:33 -0300

    Update imagen.cpp

--- imagen.cpp
+++ imagen.cpp
@@ -1,1 +1,34 @@
+#include "imagen.h"
+#include <string>
 
+using namespace std; 
+
+Imagen::Imagen(){
+    this->inactiva.ReadFromFile("inactiva.bmp");
+    this->avion.ReadFromFile("avion.bmp");
+    this->barco.ReadFromFile("barco.bmp");
+    this->soldado.ReadFromFile("soldado.bmp");
+    this->estado.SetBitDeph(24);
+}
+
+void Imagen::definirMapa(string background){
+  if (background == 'background1' ││ background == 'background2' ││ background == 'background3'){
+    this->background.ReadfromFile( background + ".bmp"); 
+ }
+  
+void Imagen::medidaDeEstado(){
+  this->estado.SetSize( this->background.TellWidth(), this->background.TellHeight()); 
+}
+ 
+void Imagen::agregarFondoAEstado(){
+  RangedPixelToPixelCopy(this->background,0,this->backgroun.TellWidth(),
+                         this->background.TellHeight(),0
+                         this->estado,0,0); 
+}
+
+void obtenerEstado(Jugador *jugador){
+  this->estado.WriteFromFile("estadoTableroDe"+ jugador->getNombre + ".bmp")
+}
+
+ 
+  
