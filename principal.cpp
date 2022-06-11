8c45e2d6f919934ba7b55411c304d2412a983c3e
joacomelli <106452464+joacomelli@users.noreply.github.com>
2022-06-11 15:56:49 -0300

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -2,13 +2,13 @@
 #include "BatallaCampal.h"
 
 int main(){
-/*
+	
 	BatallaCampal * batallaCampal = new BatallaCampal();
-	batallaCampal->iniciar();
+	
 	batallaCampal->jugar();
 
 	delete batallaCampal;
-*/
+	
 	return 0;
 }
 
