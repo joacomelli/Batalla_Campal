dbb141a730ce28477550b1f587212579a12bd3d5
agusfffff <106780131+agusfffff@users.noreply.github.com>
2022-06-11 05:47:00 -0300

    Update Imagen.cpp

--- Imagen.cpp
+++ Imagen.cpp
@@ -43,10 +43,21 @@ void EstadoTablero::agregarBarcoAEstado(int coordenadaX, int coordenadaY){
 						85 + coordenadaX*40,*this->barco(0,0) );
 }
 
-void EstadoTablero::agregarInactivaAEstado(int coordenadaX, int coordenadaY){
+//mejor un bool en vez de string pero no tengo ganas de pensar
+void EstadoTablero::agregarInactivaAEstado(int coordenadaX, int coordenadaY, in coordenadaZ, string plano){
+	if (plano == 'XY'){
 		RangedPixelToPixelCopyTransparent( this->inactiva, 0, this->inactiva.TellWidth(),
 				this->inactiva.TellHeight(), 0,this->estado, 116+ coordenadaY*40,
 				85 + coordenadaX*40,*this->inactiva(0,0) );
+	} else if (plano == 'XYZ'){
+		RangedPixelToPixelCopyTransparent( this->inactiva, 0, this->inactiva.TellWidth(),
+						this->inactiva.TellHeight(), 0,this->estado, 1030 - coordenadaZ*40,
+						85 + coordenadaX*40,*this->inactiva(0,0) );
+
+		RangedPixelToPixelCopyTransparent( this->inactiva, 0, this->inactiva.TellWidth(),
+						this->inactiva.TellHeight(), 0,this->estado, 116 + coordenadaY*40,
+						995 - coordenadaZ*40,*this->inactiva(0,0) );
+	}	
 }
 
 void Imagen::definirMapa(string background){
@@ -62,40 +73,56 @@ void Imagen::definirMapa(string background){
 void Imagen::obtenerEstadoTablero( Tablero * tablero, Jugador *jugador){
   this->medidaDeEstado();
   this->agregarFondoAEstado();
-  this->estado.WriteToFile("estadoTableroDe"+ jugador->getNombre + ".bmp");
+  this->recorrerTablero(tablero, jugador);
+  string fileName = "estadoTableroDe"+ jugador->getNombre + ".bmp";
+  this->estado.WriteToFile(fileName.c_str());
 }
 
-//||||||||||||||||||||||||||||||||||||||||||
 //Lista<Lista<Lista<Casilla *> *> *> casillas
-    void Imagen::recorrerTablero(Tablero tablero){
+void Imagen::recorrerTablero(Tablero * tablero, Jugador * jugador){
         tablero->getCasilleros()->iniciarCursor();
         while(tablero->getCasilleros()->avanzarCursor()){
-            <Lista<Lista<Casilla *> *> casillasPlanoXY = tablero->getCasilleros()->obtenerCursor();//PLANO XY
-            //Casilla casilla = recorrerPlanoXY(casillasPlanoXY);
-            recorrerPlanoXY(casillasPlanoXY);
+            <Lista<Lista<Casilla *> *> * casillasPlanoXY = tablero->getCasilleros()->obtenerCursor();//PLANO XY
+            this->recorrerPlanoXY(casillasPlanoXY, jugador);
         }
     }
 
-    void Imagen::recorrerPlanoXY(<Lista<Lista<Casilla *> *> casillas){
+void Imagen::recorrerPlanoXY(<Lista<Lista<Casilla *> *> * casillas, Jugador * jugador){
         casillas->iniciarCursor();
         while(casillas->avanzarCursor()){
-            <Lista<Casilla *> casillasRecta = casillas->obtenerCursor();
-            //Casilla casilla = recorrerCasilla();
-            recorrerCasilla();
+            <Lista<Casilla *> * casillasRecta = casillas->obtenerCursor();
+            this->recorrerCasilla(casillasRecta, jugador);
         }
     }
 
-    void Imagen::recorrerCasilla(<Lista<Casilla *> casillas, Jugador jugadorActual){
+void Imagen::recorrerCasilla(<Lista<Casilla *> * casillas, Jugador * jugadorActual){
         casillas->iniciarCursor();
         while(casillas->avanzarCursor()){
-            Casilla* casilla = casillas->obtenerCursor();
-            if((casilla->getEstado() == Ocupada) && (getFicha()->getJugador()->getNombre() == jugadorActual->getNombre())){
-                //printear casilla en el tablero
+            Casilla * casilla = casillas->obtenerCursor();
+            if((casilla->estaOcupado()) && (casilla->getFicha()->getJugador()->getNombre() == jugadorActual->getNombre()) 
+	       && (casilla->getFicha()->getEstado() == Activa ){
+		TipoDeFicha tipo = casilla->getFicha()->getTipo();
+               	this->chequearFicha(tipo, casilla); 
             }
             else if(casilla->getEstado() == Inactiva){
-                //printear casilla inactiva
+            	if (casilla->getZ() == 1){
+			agregarInactivaAEstado(casilla->getX(), casilla->getY(),casilla->getZ(), "XY");
+		} else if (casilla->getFicha() != NULL)	{
+			agregarInactivaAEstado(casilla->getX(), casilla->getY(),casilla->getZ(), "XYZ");
+		//la casilla esta inactiva pero podria haber estado vacia antes (creo) esto se fija q si devuelve
+		// null el get ficha es xq taba vacia antes y no tenia ninguna ficha, en cambio al ser z mayor a 1 
+		// son solo aviones las posibles fichas y como (creo) queremos mostrar solo las casillas inactivas del plano
+		//ZY y ZX q taban ocupados x aviones, esto hace eso. 
             }
         }
     }
  
-  
+void Imagen chequearFicha(TipoDeFicha tipo, Casilla * casilla){
+	if (tipo == FAvion) {
+		this->agregarAvionAEstado(casilla->getX(), casilla->getY(),casilla->getZ());
+	} else if (tipo == FSoldado){
+		this->agregarSoldadoAEstado(casilla->getX(), casilla->getY());
+	} else if (tipo == FBarco) 
+		this->agregarBarcoAEstado(casilla->getX(), casilla->getY());
+	}
+}	
