3c870ebd879a0ee684b30a3c3e524e0bdb0e3513
RichmondAlan <106933252+RichmondAlan@users.noreply.github.com>
2022-06-16 21:05:36 -0300

    Update Imagen.h

--- Imagen.h
+++ Imagen.h
@@ -11,29 +11,69 @@
 
 
 class Imagen {
-  private:
-    BMP background; 
-    BMP inactiva;
-    BMP avion; 
-    BMP barco;
-    BMP soldado;
-    BMP estado;
-    void medidaDeEstado();
-    void agregarFondoAEstado();
-    void agregarAvionAEstado(int coordenadaX, int coordenadaY, int coordenadaZ);
-    void agregarBarcoAEstado(int coordenadaX, int coordenadaY);
-    void agregarSoldadoAEstado(int coordenadaX, int coordenadaY);
-    void agregarInactivaAEstado(int coordenadaX, int coordenadaY, int coordenadaZ);
-    void recorrerTablero(Tablero * tablero, Jugador * jugador);
-    void recorrerPlanoXY(Lista<Lista<Casilla *> *> *casillas, Jugador * jugador);
-    void recorrerCasilla(Lista<Casilla *>* casillas, Jugador * jugadorActual);
-    void chequearFicha(Casilla * casilla);
-
+private:
+ 
+  BMP background; 
+  BMP inactiva;
+  BMP avion; 
+  BMP barco;
+  BMP soldado;
+  BMP estado;
+  
+  //PRE:
+  //POST:
+  void medidaDeEstado();
+  
+  //PRE:
+  //POST:
+  void agregarFondoAEstado();
+  
+  //PRE:
+  //POST:
+  void agregarAvionAEstado(int coordenadaX, int coordenadaY, int coordenadaZ);
+  
+  //PRE:
+  //POST:
+  void agregarBarcoAEstado(int coordenadaX, int coordenadaY);
+  
+  //PRE:
+  //POST:
+  void agregarSoldadoAEstado(int coordenadaX, int coordenadaY);
+  
+  //PRE:
+  //POST:
+  void agregarInactivaAEstado(int coordenadaX, int coordenadaY, int coordenadaZ);
+  
+  //PRE:
+  //POST:
+  void recorrerTablero(Tablero * tablero, Jugador * jugador);
+  
+  //PRE:
+  //POST:
+  void recorrerPlanoXY(Lista<Lista<Casilla *> *> *casillas, Jugador * jugador);
+  
+  //PRE:
+  //POST:
+  void recorrerCasilla(Lista<Casilla *>* casillas, Jugador * jugadorActual);
+  
+  //PRE:
+  //POST:
+  void chequearFicha(Casilla * casilla);
 
 public:
+  
+  //PRE:
+  //POST:
   Imagen();
+  
+  //PRE:
+  //POST:
   void definirMapa(std::string background); 
+  
+  //PRE:
+  //POST:
   void obtenerEstadoTablero(Tablero * tablero,Jugador * jugador);
+
 };
 
 #endif
